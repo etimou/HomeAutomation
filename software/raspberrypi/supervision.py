b1dfff0ad74882e2a44ea06676391a24e056a102
etimou <etienne.mouragnon@gmail.com>
2018-10-11 20:53:34 +0200

    update

--- software/raspberrypi/supervision.py
+++ software/raspberrypi/supervision.py
@@ -7,13 +7,22 @@ import time
 import sqlite3
 
 conn = sqlite3.connect('/home/eti/HomeAutomation/software/raspberrypi/MyHouse.db')
+conn.row_factory = sqlite3.Row
 c = conn.cursor()
 
 db = c.execute('SELECT * FROM sensors ORDER BY ID')
 for row in db:
 	print row
 
+#____________________________________________________________________________________
 
+db = c.execute('select * from alarm')
+[alarmActivationStatus, alarmIntrusionStatus, alarmAddressOnHome, alarmAddressOnAway, alarmAddressOff] = db.fetchone()
+
+print "Alarm activation status : "+  alarmActivationStatus
+
+
+#____________________________________________________________________________________
 
 
 ser = serial.Serial('/dev/ttyUSB0', 57600)
@@ -29,5 +38,31 @@ while (1):
   sql = 'UPDATE sensors SET LastSeen ="'+ time + '" WHERE address = ' + str(address)
   print sql
   c.execute(sql)
+
+  if (address == alarmAddressOnHome):
+	print ("Setting Alarm ON HOME")
+	c.execute('UPDATE alarm SET activationStatus = "ON HOME"')
+	alarmActivationStatus = "ON HOME"
+  elif (address == alarmAddressOnAway):
+	print ("Setting Alarm ON AWAY")
+	c.execute('UPDATE alarm SET activationStatus = "ON AWAY"')
+	alarmActivationStatus = "ON AWAY"
+  elif (address == alarmAddressOff):
+	print ("Setting Alarm OFF")
+	c.execute('UPDATE alarm SET activationStatus = "OFF"')
+	alarmActivationStatus = "OFF"
+	alarmIntrusionStatus = "NO"
+  
+  sql ='SELECT * FROM sensors WHERE address = ' + str(address)
+  db = c.execute(sql)
+  db = db.fetchone()
+  if (alarmActivationStatus == "ON HOME" and db["InAlarmHome"]) or (alarmActivationStatus == "ON AWAY" and db["InAlarmAway"]):
+  	print "intrusion detectee"
+  	alarmIntrusionStatus = "YES"
+
   conn.commit()
+
+
+
+
 
