2d37e45b32f1c7aa730fe4ac5ab9ee59bd3721ae
etimou <etienne.mouragnon@gmail.com>
2018-10-19 19:07:37 +0200

    modified actuators

--- software/raspberrypi/www/cgi-bin/actuators.py
+++ software/raspberrypi/www/cgi-bin/actuators.py
@@ -4,12 +4,21 @@ from topbar import printTopBar
 import sqlite3
 import os
 
+
+command= os.environ['QUERY_STRING']
+if len(command)>=8:
+	command = 'echo "' + command[8:].replace('%3B',';') + '" > /home/eti/HomeAutomation/software/raspberrypi/out.log'
+	os.system(command)
+
+
 print "Content-Type: text/html\n\n"
 print '<html><head><meta content="text/html; charset=UTF-8" />'
 print '<title>My House</title><p>'
 print '<body>'
 
 printTopBar()
+
+print command
 
 print '<table border="1" style="font-size:200%;">'
 print "<tr>"
@@ -20,7 +29,7 @@ print "<td>Action 2</td>"
 print "<td>Action 3</td>"
 print "</tr>"
 
-conn = sqlite3.connect('/home/eti/MyHouse.db')
+conn = sqlite3.connect('/home/eti/HomeAutomation/software/raspberrypi/MyHouse.db')
 c = conn.cursor()
 
 for row in c.execute('SELECT * FROM actuators ORDER BY ID'):
@@ -43,21 +52,20 @@ for row in c.execute('SELECT * FROM actuators ORDER BY ID'):
         print "</td>"
 
 	print "<td>"
-	print '<form action="actuators.py" method="GET">'
-  	print '<input type="hidden" name="command" value="'+row[7]+'">'
-  	print '<input type="submit" value="'+row[6]+'">'
-	print '</form>'
+	if row[6]!=None:
+		print '<form action="actuators.py" method="GET">'
+  		print '<input type="hidden" name="command" value="'+row[7]+'">'
+  		print '<input type="submit" value="'+row[6]+'">'
+		print '</form>'
         print "</td>"
 
 
 	print "</tr>"
 
-qs = os.environ['QUERY_STRING']
-print qs
-
 conn.close()
 
 print "</table>" 
+
 
 
 print "</body></html>"
