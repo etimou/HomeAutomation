1e011dbff725bf3a0ee28d1a00cc6dab2cc405ff
etimou <etienne.mouragnon@gmail.com>
2019-01-08 19:59:16 +0100

    include option to calibrate aand set ID at startup

--- software/arduino/NetSensor/NetSensor.ino
+++ software/arduino/NetSensor/NetSensor.ino
@@ -59,7 +59,42 @@ void setup() {
   tempSensorAvailable = tempSensor.begin();
 
   
-  Serial.println("Initialisation complete."); delay(100);
+  Serial.println("Initialisation complete.");
+
+  // calibration and Id
+  char readchar[5];
+  char nchar=0;
+  while (millis()<5000){
+    while (Serial.available()>0){
+      if (nchar<=4){
+        readchar[nchar]=Serial.read();
+        nchar++;
+      }
+      if (nchar==5){
+        if (readchar[0]=='s' && readchar[3]=='c'){
+          char newID = (readchar[1]-48)*10+readchar[2]-48;
+          if (newID){
+            saveIdToEEPROM((readchar[1]-48)*10+readchar[2]-48);
+          }
+          if ((readchar[4]-48)==1){
+            calibrationVoltage(3300);
+          }
+        }
+        else{
+          Serial.print("Wrong command...");
+        }
+        Serial.println("Starting main program");
+
+        delay(50);
+        while (Serial.available()>0){Serial.read();}
+        
+        return;
+      }
+    } 
+  }
+  Serial.println("No command received... Starting main program");
+
+  delay(100);
 
 }
 
@@ -83,6 +118,7 @@ void loop() {
     hwSleep(0, CHANGE, 1, CHANGE, SLEEP_TIME);
   }
 }
+
 
 void sendRadioData(){
     //Power up the radio
@@ -235,4 +271,51 @@ double getInternalTemp(void)
 
   return (t);
 
+}
+
+void saveIdToEEPROM(byte IdValue){
+  Serial.print("Storing sensor ID value in EEPROM: ");
+  Serial.println(IdValue);
+  EEPROM.write(0, IdValue);
+}
+void calibrationVoltage(int milliVolt){
+  // Read 1.1V reference against AVcc
+  // set the reference to Vcc and the measurement to the internal 1.1V reference
+  #if defined(__AVR_ATmega32U4__) || defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)
+    ADMUX = _BV(REFS0) | _BV(MUX4) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
+  #elif defined (__AVR_ATtiny24__) || defined(__AVR_ATtiny44__) || defined(__AVR_ATtiny84__)
+    ADMUX = _BV(MUX5) | _BV(MUX0);
+  #elif defined (__AVR_ATtiny25__) || defined(__AVR_ATtiny45__) || defined(__AVR_ATtiny85__)
+    ADMUX = _BV(MUX3) | _BV(MUX2);
+  #else
+    ADMUX = _BV(REFS0) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
+  #endif  
+
+  delay(70); // Wait for Vref to settle
+  ADCSRA |= _BV(ADSC); // Start conversion
+  while (bit_is_set(ADCSRA,ADSC)); // measuring
+
+  uint8_t low  = ADCL; // must read ADCL first - it then locks ADCH  
+  uint8_t high = ADCH; // unlocks both
+
+  long result = (high<<8) | low;
+
+  result = 1125300L / result; // Calculate Vcc (in mV); 1125300 = 1.1*1023*1000
+
+  Serial.print("Measured ");
+  Serial.print(result/1000.0);
+  Serial.println("V");
+
+
+  if ((result<3000) || (result>4000)){
+    Serial.println("Voltage not in acceptable range for 3.3V calibration");
+    return;
+  }
+  
+  
+  unsigned long calib = long(1125300L * float(milliVolt) / result); //write in float(...) the value you measure with a multimeter
+
+  Serial.print("Storing calibration value in EEPROM: ");
+  Serial.println(calib);
+  EEPROM.put(1,calib);
 }
